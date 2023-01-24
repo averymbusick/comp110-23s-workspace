fccde972b26020e2f5a58a951a5025ff0156f54d
Avery Busick <averymb@live.unc.edu>
2023-01-24 14:59:05 -0500

    finished ex 001

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,23 @@
+"""EX01 Chardle - A cute step toward Wordle."""
+__author__ = "730313069"
+
+word: str = input("Enter a 5 character word: ")
+if len(word) == 5:
+    letter: str = input("Enter a single character: ")
+    print("Searching for " + letter + " in " + word)
+    if letter in word:
+        if word[0] == letter:
+            print(letter + " found at index 0")
+        if word[1] == letter:
+            print(letter + " found at index 1")
+        if word[2] == letter:
+            print(letter + " found at index 2")
+        if word[3] == letter:
+            print(letter + " found at index 3")
+        if word[4] == letter:
+            print(letter + " found at index 4")
+        print(str(word.count(letter)) + " instances of " + letter + " found in " + word)
+    else:
+        print("No instances of " + letter + " found in " + word)
+else:
+    print("Error: Word must contain 5 characters")
