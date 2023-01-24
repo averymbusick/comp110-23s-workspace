19e0351e30a746f2382feb07533f4376b7e858a0
Avery Busick <averymb@live.unc.edu>
2023-01-24 15:30:44 -0500

    finished 01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,7 +5,7 @@ word: str = input("Enter a 5 character word: ")
 if len(word) == 5:
     letter: str = input("Enter a single character: ")
     if len(letter) != 1:
-        print ("Error: Character must be a single character")
+        print("Error: Character must be a single character")
         exit()
     print("Searching for " + letter + " in " + word)
     if letter in word:
