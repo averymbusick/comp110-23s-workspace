0f3e56fd739f4d1fb062c1cbc989990f80bf4052
Avery Busick <averymb@live.unc.edu>
2023-02-15 16:27:35 -0500

    FINISHED EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -12,28 +12,27 @@ YELLOW_BOX: str = "\U0001F7E8"
 character_exists: bool = False
 alt_index: int = 0
 
-while len(guess_word) != len(SECRET_WORD): #while loop until correct number of letters guessed
+while len(guess_word) != len(SECRET_WORD):  # while loop until correct number of letters guessed
     guess_word = input(f"That was not { len(SECRET_WORD) } letters! Try again: ")
 if guess_word == SECRET_WORD:
     print(int(len(SECRET_WORD)) * GREEN_BOX)
     print("Woo! You got it! ")
-    exit()
-else: # correct letter word that isnt the secret
+else:  # correct letter word that isnt the secret
     while index < len(SECRET_WORD):
-            if SECRET_WORD[index] == guess_word[index]: #matching letters, same index
-                        emoji_string += GREEN_BOX
-            else: #letters dont match @ index
-                        character_exists = False
-                        alt_index = 0
-                        while alt_index < len(SECRET_WORD) and character_exists is False: 
-                            if SECRET_WORD[alt_index] == guess_word[index]:
-                                character_exists = True
-                            else:
-                                alt_index += 1
-                        if character_exists != True:
-                            emoji_string += WHITE_BOX
-                        else:
-                            emoji_string += YELLOW_BOX
-            index += 1 #check 0 indice, then 1, 2, etc. until = len(secret_word)
+        if SECRET_WORD[index] == guess_word[index]:  # matching letters, same index
+            emoji_string += GREEN_BOX
+        else:  # letters dont match @ index
+            character_exists = False
+            alt_index = 0
+            while alt_index < len(SECRET_WORD) and character_exists is False: 
+                if SECRET_WORD[alt_index] == guess_word[index]:
+                    character_exists = True
+                else:
+                    alt_index += 1
+            if character_exists is False:
+                emoji_string += WHITE_BOX
+            else:
+                emoji_string += YELLOW_BOX
+        index += 1  # check 0 indice, then 1, 2, etc. until = len(secret_word)
     print(emoji_string)
-print("Not quite. Play again soon! ")   
+    print("Not quite. Play again soon! ")   
