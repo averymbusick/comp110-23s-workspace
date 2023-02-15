571c0f48fd519d4d01868267751c834c324de15d
Avery Busick <averymb@live.unc.edu>
2023-02-15 15:32:52 -0500

    EX02 again

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,31 +1,38 @@
 """EX02: One shot wordle."""
+
 __author__ = "730313069"
 
-secret_word: str = "python"
-guess_word: str = input(f"What is your { len(secret_word) }-letter guess? ")
+SECRET_WORD: str = "python"
+
+guess_word: str = input(f"What is your { len(SECRET_WORD) }-letter guess? ")
+
 index: int = 0
+
 emoji_string: str = ""
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
+
 character_exists: bool = False
+
 alt_index: int = 0
 
-while len(guess_word) != len(secret_word): #while loop until correct number of letters guessed
-   guess_word: str = input(f"That was not { len(secret_word) } letters! Try again: ")
-if guess_word == secret_word:
-        print(int(len(secret_word)) * GREEN_BOX)
+while len(guess_word) != len(SECRET_WORD): #while loop until correct number of letters guessed
+   guess_word: str = input(f"That was not { len(SECRET_WORD) } letters! Try again: ")
+if guess_word == SECRET_WORD:
+        print(int(len(SECRET_WORD)) * GREEN_BOX)
         print("Woo! You got it! ")
         exit()
 else: # correct letter word that isnt the secret
-        while index < len(secret_word):
-                if secret_word[index] == guess_word[index]: #matching letters, same index
+        while index < len(SECRET_WORD):
+                if SECRET_WORD[index] == guess_word[index]: #matching letters, same index
                         emoji_string += GREEN_BOX
                 else: #letters dont match @ index
                         character_exists = False
                         alt_index = 0
-                        while alt_index < len(secret_word) and character_exists is False: 
-                                if secret_word[alt_index] == guess_word[index]:
+                        while alt_index < len(SECRET_WORD) and character_exists is False: 
+                                if SECRET_WORD[alt_index] == guess_word[index]:
                                         character_exists = True
                                 else:
                                         alt_index += 1
